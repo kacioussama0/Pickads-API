59f4004dd42c72840c0babc8d5cec6b7a046f901
unknown <kacioussama0@gmail.com>
2023-10-13 20:46:36 +0100

    some-update

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware(['basicAuth'])->group(function () {
     Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
     Route::get('/influencers', [App\Http\Controllers\HomeController::class, 'influencers'])->name('influencers');
-    Route::resource('ads', \App\Http\Controllers\AdController::class)->middleware('');
+    Route::resource('ads', \App\Http\Controllers\AdController::class);
     Route::resource('categories', \App\Http\Controllers\CategoryController::class);
     Route::resource('social-media', \App\Http\Controllers\SocialMediaController::class);
 }
