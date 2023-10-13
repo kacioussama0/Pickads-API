654cb70421757b46044fed6a649c8b9c2823e1e7
unknown <kacioussama0@gmail.com>
2023-10-13 20:36:37 +0100

    some-update

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -65,6 +65,7 @@ class Kernel extends HttpKernel
         'signed' => \App\Http\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
-        'isAdmin' => \App\Http\Middleware\IsAdmin::class
+        'isAdmin' => \App\Http\Middleware\IsAdmin::class,
+        'basicAuth' => \App\Http\Middleware\BasicAuth::class
     ];
 }
--- routes/web.php
+++ routes/web.php
@@ -13,11 +13,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-Route::get('/influencers', [App\Http\Controllers\HomeController::class, 'influencers'])->name('influencers');
-
-Route::resource('ads', \App\Http\Controllers\AdController::class);
-Route::resource('categories', \App\Http\Controllers\CategoryController::class);
-Route::resource('social-media', \App\Http\Controllers\SocialMediaController::class);
+Route::middleware(['basicAuth'])->group(function () {
+    Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+    Route::get('/influencers', [App\Http\Controllers\HomeController::class, 'influencers'])->name('influencers');
+    Route::resource('ads', \App\Http\Controllers\AdController::class)->middleware('');
+    Route::resource('categories', \App\Http\Controllers\CategoryController::class);
+    Route::resource('social-media', \App\Http\Controllers\SocialMediaController::class);
+}
+);
